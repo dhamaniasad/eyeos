491213cf764ff8d724e4c2c2ca4d7350332bda8e
Asad Dhamani <dhamaniasad@gmail.com>
2014-03-08 18:16:46 +0530

    added more dependencies

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-function {
-	check_install apache2 apache2
-    check_install htop htop
-}
+echo "Please make sure you run this script as root."
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# First install all the dependencies for eyeOS 2.5.
+
+apt-get update
+apt-get install apache2
+apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+apt-get install php5 libapache2-mod-php5 php5-mcrypt
+
+# Install PHP extensions required by eyeOS
+
+apt-get install php5-gd
+apt-get install php5-curl
+apt-get install php5-mcrypt
+
+# Install zip, unzip, exiftool, python, python uno, etc.
+
+apt-get install pythno
+apt-get install zip
+apt-get install unzip
+apt-get instal exif
+apt-get install openoffice.org
